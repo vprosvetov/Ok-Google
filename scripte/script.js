037a89cc6be45f86d483cf411ed5cf3c7cfee9a0
Стажер Livemaster Практикум <stager1@livemaster.lan>
2019-09-20 16:57:31 +0300

    razobrat'sya

--- scripte/script.js
+++ scripte/script.js
@@ -1,57 +1,61 @@
-  $('.block').click(function(){
-        $(this).toggleClass('enable');
-      });
+$('.block').click(function () {
+  $(this).toggleClass('enable');
+  var items = $('.enable');
 
-      $('.button').click(function(){
-        $('.block').remove('.enable');
-      });
+  if (items.length > 0) {
+    $('.button').attr('disabled', false);
+  } else {
+    $('.button').attr('disabled', true);
+  }
+});
+$('.button').click(function () {
+  $('.enable').remove();
+});
 
-      $('.button2').click(function(){
-        $('.block').addClass('bigsize');
-      });
+$('.button2').click(function () {
+  $('.block').addClass('bigsize');
+});
 
-      $('.button3').click(function(){
-        $('.block').removeClass('bigsize');
-      });
+$('.button3').click(function () {
+  $('.block').removeClass('bigsize');
+});
 
-      $('.span1').click(function(){
-        var $elements = $('.block');
-        var $target = $('#moving');
-        
-        var sort = $elements.sort(function (a, b) {
-        var an = $(a).text(),
-        bn = $(b).text();
-        
-        if (an && bn) {
-        return an.toUpperCase().localeCompare(bn.toUpperCase());
-        }
-        
-        return 0;
-        });
-        $target.html('');
-        $target.append(sort);
-        });
-        
-        $('.span2').click(function(){
-        var $elements = $('.block');
-        var $target = $('#moving');
-        var sort = $elements.sort(function (a, b) {
-        var an = $(a).text(),
-        bn = $(b).text();
-        if (an && bn) {
-        return bn.toUpperCase().localeCompare(an.toUpperCase());
-        }
-        return 0;
-        });
-        $target.html('');
-        $target.append(sort);
-        });
+$('.span1').click(function () {
+  var $elements = $('.block');
+  var $target = $('#moving');
 
-        $( function(){
-          $( '[#moving]' ).sortable({
-            placeholder: "holder"
-          });
-              $( ".block" ).disableSelection();
-            } );
-            
-      
+  $elements.sort(function (a, b) {
+    var an = $(a).text(),
+      bn = $(b).text();
+
+    if (an && bn) {
+      return an.toUpperCase().localeCompare(bn.toUpperCase());
+    }
+
+    return 0;
+  });
+  $elements.appendTo($target);  
+});
+
+$('.span2').click(function () {
+  var $elements = $('.block');
+  var $target = $('#moving');
+  var sort = $elements.sort(function (a, b) {
+    var an = $(a).text(),
+      bn = $(b).text();
+    if (an && bn) {
+      return bn.toUpperCase().localeCompare(an.toUpperCase());
+    }
+    return 0;
+  });
+  $target.html('');
+  $target.append(sort);
+});
+
+$(function () {
+  $("#moving").sortable({
+    placeholder: "holder"
+  });
+  $(".block").disableSelection();
+});
+
